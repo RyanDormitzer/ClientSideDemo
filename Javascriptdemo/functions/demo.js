8733bf83960d44737b16714b2a1c8d0828e2f7ac
RYan Dormitzer <dormitzerryan@gmail.com>
2017-06-28 11:25:29 -0400

    jsdemo

---
+++ Javascriptdemo/functions/demo.js
@@ -0,0 +1,14 @@
+console.log('test');
+var c = console;
+
+//a  function
+function log(val){
+    c.log(val);
+}
+
+// executed function
+//log('hello');
+//log(1);
+
+var elem = document.getElementById("main");
+log(elem);
