bfd12b59a7169ff0d84d3049adcbc47e9529fef8
RYan Dormitzer <dormitzerryan@gmail.com>
2017-06-28 11:32:33 -0400

    jsdemo 2

--- Javascriptdemo/functions/demo.js
+++ Javascriptdemo/functions/demo.js
@@ -1,8 +1,15 @@
 console.log('test');
 var c = console;
 
+//files when DOM is rendered
+window.onload = function() 
+{
+    var elem = document.getElementById("main");
+    log(elem);
+}
 //a  function
-function log(val){
+function log(val)
+{
     c.log(val);
 }
 
@@ -10,5 +17,6 @@ function log(val){
 //log('hello');
 //log(1);
 
+// executed before page is done
 var elem = document.getElementById("main");
 log(elem);
