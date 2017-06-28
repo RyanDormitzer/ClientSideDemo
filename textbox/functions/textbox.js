3e0fa6b06f10ce3262bb627fbec183af5dbcc33d
RYan Dormitzer <dormitzerryan@gmail.com>
2017-06-28 14:44:44 -0400

    calc

---
+++ textbox/functions/textbox.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var c = console;
+
+window.onload = function()
+{
+    var btnElem = document.getElementById('btnGo');
+    var inputElem = document.getElementById('tb');
+
+    var clog = function(){
+        c.log(inputElem.value);
+
+    }
+
+    //add click event to button
+    btnElem.addEventListener("click", clog);
+    btnElem.addEventListener('click', function(event){
+        var numElem = document.getElementById('num');
+        var val = numElem.value;
+        c.log(val);
+        c.log(typeof val);
+
+        var num = parseInt(val);
+        c.log(num);
+        c.log(typeof num);
+    });
+    inputElem.addEventListener('keypress', clog);
+};
