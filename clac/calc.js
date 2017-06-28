973d7ad78355cff7df660ed0295e9a8aa1e7b45e
RYan Dormitzer <dormitzerryan@gmail.com>
2017-06-28 16:34:48 -0400

    calc2

--- clac/calc.js
+++ clac/calc.js
@@ -4,65 +4,39 @@ var c = console;
 
 window.onload = function()
 {
-    var btnElem = document.getElementById('btnGo');
-    var inputElem = document.getElementById('num');
-     var inputElem1 = document.getElementById('num1');
-     var addc = document.getElementById('add');
-     var subc = document.getElementById('sub');
-     var mulc = document.getElementById('mul');
-     var divc = document.getElementById('div');
-    var clog = function(){
-        c.log(inputElem.value);
-    }
-    var clog1 = function(){
-        c.log(inputElem1.value);
-    }
-    var clogadd = function(){
-        c.log(addc.value);
-    }
-    var clogsub = function(){
-        c.log(subc.value);
-    }
-    
-    var clogmul = function(){
-        c.log(mulc.value);
-    }
-    
-    var clogdiv = function(){
-        c.log(divc.value);
-    }
-    //add click event to button
-    btnElem.addEventListener("click", clog);
-    btnElem.addEventListener('click', function(event){
-        var numElem = document.getElementById('num');
-        var val = numElem.value;
-        var numElem1 = document.getElementById('num1');
-        var val1 = numElem1.value;
+    var num1 = document.getElementById('tb');
+    var num2 = document.getElementById('tb1');
+    var add = document.getElementById('add');
+    var sub = document.getElementById('sub');
+    var mul = document.getElementById('mul');
+    var div = document.getElementById('div');
 
-        var num = parseInt(val);
-        c.log(num);
-        var num1 = parseInt(val1);
-        c.log(num1);
-    });
-    add.addEventListener("click", clogadd);
     add.addEventListener('click', function(event){
-        var add = document.getElementById('add');
-        var addval = add.value;
+    var add = num1.value;  
+    var add1 = num2.value; 
+    var a1 = parseInt(add);
+    var a2 = parseInt(add1);     
+    c.log(a1+a2);
+});
+sub.addEventListener('click', function(event){
+    var sub = num1.value;  
+    var sub1 = num2.value; 
+    var s1 = parseInt(sub);
+    var s2 = parseInt(sub1);     
+    c.log(s1-s2);
+});
+mul.addEventListener('click', function(event){
+    var mul = num1.value;  
+    var mul1 = num2.value; 
+    var m1 = parseInt(mul);
+    var m2 = parseInt(mul1);     
+    c.log(m1*m2);
+});
+div.addEventListener('click', function(event){
+    var div = num1.value;  
+    var div1 = num2.value; 
+    var d1 = parseInt(div);
+    var d2 = parseInt(div1);     
+    c.log(d1/d2);
     });
-    sub.addEventListener('click', clogsub);
-    sub.addEventListener('click', function(event){
-        var sub = document.getElementById('sub');
-        var subval = sub.value;
-    });
-    mul.addEventListener('click', clogmul);
-    mul.addEventListener('click', function(event){
-        var mul = document.getElementById('mul');
-        var mulval = mul.value;
-    });
-    div.addEventListener('click', clogdiv);
-    div.addEventListener('click', function(event){
-        var div = document.getElementById('div');
-        var divval = div.value;
-    });
-    inputElem.addEventListener('keypress', clog);
 };
