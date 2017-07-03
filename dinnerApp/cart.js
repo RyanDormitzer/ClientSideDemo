7feae81ba1e22500ed682c2f37c0fc97d7484cd9
RYan Dormitzer <dormitzerryan@gmail.com>
2017-07-03 15:44:39 -0400

    DinerApp

--- dinnerApp/cart.js
+++ dinnerApp/cart.js
@@ -3,6 +3,13 @@ var c = console;
 
 window.onload = function(){
 var total = 0.00;
-
+var Nach = document.getElementById('Nachos');
+var Mot  = document.getElementById('Mot');
+var burg = document.getElementById('burg');
+var tom = document.getElementById('tom');
+var soda = document.getElementById('soda');
+var tea = document.getElementById('tea');
+var cart = document.getElementById('cart');
+var add = document.getElementById('add');
 }
 
