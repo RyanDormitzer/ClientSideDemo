990e4b34746a8efac935833438398bca3ce26f38
RYan Dormitzer <dormitzerryan@gmail.com>
2017-07-05 10:20:27 -0400

    DinerApp

--- dinnerApp/cart.js
+++ dinnerApp/cart.js
@@ -11,5 +11,11 @@ var soda = document.getElementById('soda');
 var tea = document.getElementById('tea');
 var cart = document.getElementById('cart');
 var add = document.getElementById('add');
+var NachP = 5.99;
+var MotP = 5.00;
+var burgP = 8.99;
+var tomP = 7.99;
+var sodaP = 1.99;
+var teaP = 2.00;
 }
 
