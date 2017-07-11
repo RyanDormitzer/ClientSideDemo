bcf4ae758e4cb43ab93c84f7ed12deb596cbc367
RYan Dormitzer <dormitzerryan@gmail.com>
2017-07-11 09:52:32 -0400

    test

--- dinnerApp/cart.js
+++ dinnerApp/cart.js
@@ -13,85 +13,105 @@ var addM = document.getElementById('addM');
 var addB = document.getElementById('addB');
 var addT = document.getElementById('addT');
 var addS = document.getElementById('addS');
-var addTe = document.getElementById('addte');
+var addTe = document.getElementById('addTe');
+var tota = document.getElementById('tota');
+var getT = document.getElementById('getT');
+var del = document.createElement('button');
+del.innerText='Delete';
 var Ocart = [];
-var NachP = 5.99;
-var MotP = 5.00;
-var burgP = 8.99;
-var tomP = 7.99;
-var sodaP = 1.99;
-var teaP = 2.00;
 var cartT =[];
-var k = 0;
 window.onload = function(){
 
-function getTotal(total)
+/*function getTotal(total)
 {
     for(var i=0; i < cartT.length; i++)
     {
-        total = total + cartT[this[i]];
+        total = total + cartT[i];
     }
     return(total);
-}
+} */
 
 
-    var item = document.createElement('li');
+   
     addN.addEventListener('click', function(event)
     {
-        item.innerText=addN.innerText;
-        cart.appeendChild(item)
-        push.cartT(5.00);
-        push.Ocart(addN.innerText);
-        k++;
-    })
+        var item = document.createElement('li');
+        item.innerText=Nach.innerText;
+        cart.appendChild(item);
+      //  cart.appendChild(del)
+        cartT.push(5.99);
+        Ocart.push(item.innerText);
+    });
     addM.addEventListener('click', function(event)
     {
-        item.innerText=addM.innerText;
-        cart.appeendChild(item)
-        push.cartT(5.99);
-        push.Ocart(addM.innerText);
-        k++;
-    })
+         var item = document.createElement('li');
+        item.innerText=Mot.innerText;
+        cart.appendChild(item);
+       // cart.appendChild(del)
+        cartT.push(5.00);
+        Ocart.push(item.innerText);
+    });
     addB.addEventListener('click', function(event)
     {
-        item.innerText=addB.innerText;
-        cart.appeendChild(item)
-        push.cartT(8.99);
-        push.Ocart(addB.innerText);
-        k++;
-    })
+         var item = document.createElement('li');
+        item.innerText=burg.innerText;
+        cart.appendChild(item);
+       // cart.appendChild(del)
+        cartT.push(8.99);
+        Ocart.push(item.innerText);
+    });
     addT.addEventListener('click', function(event)
     {
-        item.innerText=addT.innerText;
-        cart.appeendChild(item)
-        push.cartT(7.99);
-        push.Ocart(addT.innerText);
-        k++;
-    })
+         var item = document.createElement('li');
+        item.innerText=tom.innerText;
+        cart.appendChild(item);
+      //  cart.appendChild(del)
+        cartT.push(7.99);
+        Ocart.push(item.innerText);
+    });
 addS.addEventListener('click', function(event)
     {
-        item.innerText=addS.innerText;
-        cart.appeendChild(item)
-        push.cartT(1.99);
-        push.Ocart(addS.innerText);
-        k++;
-    })
+         var item = document.createElement('li');
+        item.innerText=soda.innerText;
+        cart.appendChild(item);
+       // cart.appendChild(del);
+        cartT.push(1.99);
+        Ocart.push(item.innerText);
+    });
     addTe.addEventListener('click', function(event)
     {
-        item.innerText=addTe.innerText;
-        cart.appeendChild(item)
-        push.cartT(2.00);
-        push.Ocart(addTe.innerText);
-        k++;
-    })
-    getCart function(cart){
+        var item = document.createElement('li');
+        item.innerText=tea.innerText;
+        cart.appendChild(item);
+       // cart.appendChild(del)
+        cartT.push(2.00);
+        Ocart.push(item.innerText);
+    });
+ /* getCart static function(){
+        var item = document.createElement('li');
 for(var i = 0; i < Ocart.length; i++)
 {
-    cart.appeendChild(Ocart[this[i]]);
+    item.innerText = Ocart[this[i]];
+    cart.appeendChild(item);
 }
+return Ocart;
+    }*/
+
+getT.addEventListener('click', function(event)
+{
+    total = 0;
+    //getTotal();
+    //
+     for(var i=0; i < cartT.length; i++)
+    {
+        total = total + cartT[i];
     }
-last.addEventListener('click',function(event){
-    getCart(last);
-})
+    c.log(total);
+    tota.innerText ='$' + total;
+});
+/*del.addEventListener('click', function(event)
+{
+    cart.removeChild(item);
+})*/
 }
 
